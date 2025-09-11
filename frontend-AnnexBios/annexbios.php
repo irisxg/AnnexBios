8b1dad6c48ceed4f09321e6656abd93cdc57395a
Jop-Lamers <Jop14.lamers@gmail.com>
2025-09-11 09:48:31 +0200

    alles opgedeeld

---
+++ frontend-AnnexBios/annexbios.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AnnexBios</title>
+    <link rel="stylesheet" href="annexbios.css">
+</head>
+
+<body>
+    <?php include 'header.php'; ?>
+    <?php include 'main.php'; ?>
+    <?php include 'footer.php'; ?>
+</body>
+
+</html>
---
+++ frontend-AnnexBios/main.php
@@ -0,0 +1,5 @@
+ <main>
+        <div class="zwart-vak">
+            <span class="lees-meer">lees meer</span>
+        </div>
+    </main>
